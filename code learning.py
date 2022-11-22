35d64b5648c0e5d3ce865097e7fbf2af81f84c4b
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2022-11-22 17:36:19 -0600

    Update code learning.py

--- code learning.py
+++ code learning.py
@@ -6,24 +6,32 @@
 # Set the value of f to the function. Now you can treat f like a function.
 # f = square
 
-# print out function information
+# print out function information, an entity which supports all the operations avail to other entities.
 # print(square)
 # print out the returned vaule from the square function
 # print(f(5))
 
-def square(x):
-    return x * x
+# def square(x):
+#     return x * x
 
-def my_map(func, arg_list):
-    result = []
-    for i in arg_list:
-        result.append(func(i))
-    return result
+# def cube(x):
+#     return x * x * x
+
+# def my_map(func, arg_list):
+#     result = []
+#     for i in arg_list:
+#         result.append(func(i))
+#     return result
     
-squares = my_map(square, [1, 2, 3, 4,5])
+# squares = my_map(cube, [1, 2, 3, 4, 5])
 
-print(squares)
+# print(squares)
 
-def cube(x):
-    return x * x * x
+def logger(msg):
 
+    def log_message():
+        print('Log: ', msg)
+    return log_message
+
+log_hi = logger('Hi!')
+log_hi()
