f58e32aab7f94031ee2c4de4b86afd23b0737c50
Winter Evans <41024059+TheWinterSkye@users.noreply.github.com>
2022-11-22 17:50:42 -0600

    Update code learning.py

--- code learning.py
+++ code learning.py
@@ -27,11 +27,25 @@
 
 # print(squares)
 
-def logger(msg):
+# def logger(msg):
 
-    def log_message():
-        print('Log: ', msg)
-    return log_message
+#     def log_message():
+#         print('Log: ', msg)
+#     return log_message
 
-log_hi = logger('Hi!')
-log_hi()
+# log_hi = logger('Hi!')
+# log_hi()
+
+def html_tag(tag):
+
+    def  wrap_text(msg):
+         print('<{0}>{1}</{0}>'.format(tag, msg))
+
+    return wrap_text
+
+print_h1 = html_tag('h1')
+print_h1('Text Headline!')
+print_h1('Another Headline!')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
